9e4d9906496ab07d4874ae068cb46986d4ad9df6
Nathyy21 <nathbarodrigues25@gmail.com>
2026-02-03 14:17:10 -0300

    Primeiro commit de 2026

---
+++ Back_End_2/index.php
@@ -0,0 +1,18 @@
+<?php
+    echo "<h1>Página principal do meu servidor</h1>";
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>POO</title>
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+    <body>
+        <a href="POO/index.php">Aula de POO</a>
+    </body>
+</html>
